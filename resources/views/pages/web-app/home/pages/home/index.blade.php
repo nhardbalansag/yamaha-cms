e5f7ed1f88e4b10ca0c6a3803c602b618eabe0d2
bernard balansag <nhardbalansag@gmail.com>
2020-11-29 00:36:39 +0800

    Update index.blade.php

--- resources/views/pages/web-app/home/pages/home/index.blade.php
+++ resources/views/pages/web-app/home/pages/home/index.blade.php
@@ -14,7 +14,7 @@
                         </a>
                     </div>
                     <div class="mt-2">
-                        <h5 class="truncate">{{$value->title}}</h5>
+                        <h5 class="text-truncate">{{$value->title}}</h5>
                         <p>SRP: <span>{{$value->price}}</span></p>
                         <a href="/home/product/{{$value->id}}" class="btn btn-outline-primary btn-lg w-full" role="button" aria-pressed="true">View Details</a>
                     </div>
