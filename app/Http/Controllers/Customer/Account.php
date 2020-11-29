8630a04b7a26ab7f49d836151c60f72402cafbf1
nhardbalansag <nhardbalansag@gmail.com>
2020-11-29 14:57:36 +0800

    dd on account

--- app/Http/Controllers/Customer/Account.php
+++ app/Http/Controllers/Customer/Account.php
@@ -42,18 +42,18 @@ class Account extends Controller
     }
 
     public function verify($email){
+        dd($email);
+        // $column = ['email'=> $email];
 
-        $column = ['email'=> $email];
+        // $results = User::select('email')
+        //                 ->where($column) 
+        //                 ->get();
 
-        $results = User::select('email')
-                        ->where($column) 
-                        ->get();
-
-        if($results[0]->email === $email){
-            dd("matched");
-        }else if($results[0]->email !== $email){
-            return redirect('/customer/register');
-        }
+        // if($results[0]->email === $email){
+        //     dd("matched");
+        // }else if($results[0]->email !== $email){
+        //     return redirect('/customer/register');
+        // }
                   
         
         // $affected = DB::table('users')
