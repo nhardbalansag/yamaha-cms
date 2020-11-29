b3be8542ee9cc3dcdcf776c14c59d1ba56c6bbe4
bernard balansag <nhardbalansag@gmail.com>
2020-11-29 20:43:46 +0800

    Update index.blade.php

--- resources/views/pages/web-app/home/pages/home/index.blade.php
+++ resources/views/pages/web-app/home/pages/home/index.blade.php
@@ -15,8 +15,8 @@
                     </div>
                     <div class="d-flex align-items-end">
                         <div>
-                            <div class="mb-2 font-weight-bold truncate">{{$value->title}}</div>
-                            <div class="font-weight-bold text-dark truncate">P {{$value->price}}.00</div>
+                            <div class="mb-2 font-weight-bold ">{{$value->title}}</div>
+                            <div class="font-weight-bold text-dark ">P {{$value->price}}.00</div>
                         </div>
                     </div>
                 </a>
