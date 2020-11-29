ba7b01d964b4d91c18095423d19ef8d945ad8af0
nhardbalansag <nhardbalansag@gmail.com>
2020-11-29 15:36:26 +0800

    remove admin verification

--- resources/views/livewire/home/navigation-bar.blade.php
+++ resources/views/livewire/home/navigation-bar.blade.php
@@ -41,7 +41,7 @@
           <a class="text-xl text-white nav-link" href="#" tabindex="-1" aria-disabled="true">Contact Us</a>
         </li>
         @auth
-          @if(Auth::user()->role === 'admin' && Auth::user()->verified === 1)
+          @if(Auth::user()->role === 'admin')
             <li class="nav-item active">
               <a class="text-xl text-white nav-link" href="/dashboard" tabindex="-1" aria-disabled="true">Dashboard</a>
             </li>
@@ -108,36 +108,3 @@
     
   </div>
 </div>
-
-
-
-<div class="modal" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-$(function() { alert("shortcut for document.ready method in jQuery"); });
-hello();
-function hello(){
-  alert()
-}
-</script>
