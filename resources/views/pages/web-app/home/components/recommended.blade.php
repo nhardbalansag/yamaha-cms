08de9c01bbc15695bddfc64b3a6653e1767dca58
nhardbalansag <nhardbalansag@gmail.com>
2020-11-29 21:03:53 +0800

    update ui

--- resources/views/pages/web-app/home/components/recommended.blade.php
+++ resources/views/pages/web-app/home/components/recommended.blade.php
@@ -2,25 +2,23 @@
             <div>
                 <p class = "text-4xl font-bold capitalize">recommended</p>
             </div>
-            <div class="swiper-container row">
+            <div class="swiper-container">
                 <div class="pb-20 swiper-wrapper">
                     @foreach($recommended as $key => $value)
-                        <div class="border swiper-slide">
-                            <div class="p-0 mx-2">
-                                <a href="/home/product/{{$value->id}}" class="btn btn-lg" tabindex="0">
-                                    <div class=" row">
-                                        <div class="h-64 p-0 border col-md-12 d-flex align-items-center" >
-                                            <img src="{{url('storage/' . $value->photo_path) }}" loading="lazy" alt="...">
-                                        </div>
+                        <div class="flex justify-center swiper-slide ">
+                            <a href="/home/product/{{$value->id}}" class="btn btn-lg" tabindex="0">
+                                <div class="row">
+                                    <div class="h-64 p-0 border col-12 col-md-12 d-flex align-items-center" >
+                                        <img src="{{url('storage/' . $value->photo_path) }}" loading="lazy" alt="...">
                                     </div>
-                                    <div class="d-flex align-items-end">
-                                        <div class=" col-12">
-                                            <div class="col-12 font-weight-bold text-truncate">{{$value->title}}</div>
-                                            <div class="col-12 font-weight-bold text-dark text-truncate">P {{$value->price}}.00</div>
-                                        </div>
+                                </div>
+                                <div class="d-flex align-items-end">
+                                    <div>
+                                        <div class="mb-2 font-weight-bold text-truncate">{{$value->title}}</div>
+                                        <div class="font-weight-bold text-dark ">P {{$value->price}}.00</div>
                                     </div>
-                                </a>
-                            </div>
+                                </div>
+                            </a>
                         </div>
                     @endforeach
                 </div>
