7f86a94e4f64d432cce11b008497f3aee632142b
nhardbalansag <nhardbalansag@gmail.com>
2020-12-06 21:22:39 +0800

    adding function to inquiry livewire controller for recaptcha

--- app/Http/Livewire/Home/Inquire.php
+++ app/Http/Livewire/Home/Inquire.php
@@ -54,6 +54,33 @@ class Inquire extends Component
 
     public function createInquiry(){
 
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+$remoteip = $_SERVER['REMOTE_ADDR'];
+$data = [
+        'secret' => config('services.recaptcha.secret'),
+        'response' => $request->get('recaptcha'),
+        'remoteip' => $remoteip
+      ];
+$options = [
+        'http' => [
+          'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method' => 'POST',
+          'content' => http_build_query($data)
+        ]
+    ];
+$context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $resultJson = json_decode($result);
+if ($resultJson->success != true) {
+        return back()->withErrors(['captcha' => 'ReCaptcha Error']);
+        }
+if ($resultJson->score >= 0.3) {
+        //Validation was successful, add your form submission logic here
+        return back()->with('message', 'Thanks for your message!');
+} else {
+        return back()->withErrors(['captcha' => 'ReCaptcha Error']);
+}
+
         $validatedData = $this->validate($this->data);
         Inquiry::create($validatedData);
 
