d63e4afd0d828425c6a0863928a61f94a1cb0ebc
nhardbalansag <nhardbalansag@gmail.com>
2020-12-17 18:40:04 +0800

    modification

--- resources/views/pages/client/pages/payment-result.blade.php
+++ resources/views/pages/client/pages/payment-result.blade.php
@@ -13,7 +13,9 @@
                 @endif
             </div>
             <h2>Checkout form</h2>
-            <p class="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+            <p class="lead">
+                below are the details about the product you are purchasing. Please check the details before proceeding thank you.
+            </p>
             </div>
                 <div class="alert  {{$result == true ? 'alert-success' : 'alert-danger'}}" role="alert">
                    {{$result == true ? " payment success! thank you" : "payment cannot be added please send us a proof of your payment transaction"}}
