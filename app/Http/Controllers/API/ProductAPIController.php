d5c53bc6072eff57e2249dc56df2ca2736aea892
nhardbalansag <nhardbalansag@gmail.com>
2020-12-28 08:31:37 +0800

    adding api endpoint for viewing one product

--- app/Http/Controllers/API/ProductAPIController.php
+++ app/Http/Controllers/API/ProductAPIController.php
@@ -41,13 +41,15 @@ class ProductAPIController extends Controller
             $product = Product::where('id', $request->id)->get();
             $specification = DB::select('SELECT
                                                 product_specifications.title as title,
+                                                product_specifications.id as id,
                                                 product_specifications.description as description
 
                                                 FROM product_specifications, products
                                                 WHERE (product_specifications.status = "publish") AND (product_specifications.product_id = ' .  $request->id . ')
                                                 GROUP BY
                                                 product_specifications.title,
-                                                product_specifications.description');
+                                                product_specifications.description,
+                                                product_specifications.id');
 
             $inquiries = DB::select('SELECT COUNT(id) as inquiries
                                         FROM inquiries
@@ -63,7 +65,7 @@ class ProductAPIController extends Controller
                     "product" => $product,
                     "specification" => $specification,
                     "inquiriesCount" => $inquiries[0]->inquiries,
-                    "Percentage" => $totalpercentage
+                    "Percentage" => round($totalpercentage)
                 )
             );
 
