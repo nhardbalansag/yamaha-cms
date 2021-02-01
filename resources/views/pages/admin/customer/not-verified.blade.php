6d37287c5663b8262fa64f984beb4083473dfb5d
nhardbalansag <nhardbalansag@gmail.com>
2021-02-02 01:17:10 +0800

    change table actions

--- resources/views/pages/admin/customer/not-verified.blade.php
+++ resources/views/pages/admin/customer/not-verified.blade.php
@@ -3,13 +3,13 @@
 
 
 <div class="my-4 text-center border-bottom border-dark row col-md-12 text-capitalize">
-    <div class="col-md-3">
+    <div class="col-md-2">
         name
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
         status
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
         role
     </div>
     <div class="col-md-3">
@@ -22,13 +22,13 @@
 @foreach($notVerified as $key => $value)
 
     <div class="py-2 my-2 text-center border-bottom border-dark row col-md-12">
-        <div class="col-md-3 text-truncate">
+        <div class="col-md-2 text-truncate">
             {{$value->first_name}}
         </div>
-        <div class="col-md-3 {{ $value->verified == 1 ? 'text-success' : 'text-danger' }}">
+        <div class="col-md-2 {{ $value->verified == 1 ? 'text-success' : 'text-danger' }}">
             {{$value->verified == 1 ? 'verified' : 'not verified'}}
         </div>
-        <div class="col-md-3">
+        <div class="col-md-2">
             {{$value->role }}
         </div>
         <div class="col-md-3">
