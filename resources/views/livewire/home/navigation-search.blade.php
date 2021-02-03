9ba61913b9a39b285fc2c6b4a6c2a7f6b864fa40
nhardbalansag <nhardbalansag@gmail.com>
2021-02-03 22:18:17 +0800

    fix sort search

--- resources/views/livewire/home/navigation-search.blade.php
+++ resources/views/livewire/home/navigation-search.blade.php
@@ -37,7 +37,7 @@
                         </div>
                     </div>
                     <div class="mb-6">
-                            <div class="">
+                        <div class="">
                             <label for="productCategory" class="block text-sm font-medium leading-5 text-gray-700">Category</label>
                             <select wire:model="productCategory"  id="productCategory" class="block w-full p-2 px-3 py-2 mt-1 transition duration-150 ease-in-out bg-white border-gray-400 border-solid rounded-md shadow-sm border-1 form-select focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
                                 <option value="">Select Category</option>
