dff9e61f28797c12f2e3bc61100a2d08c917532d
nhardbalansag <nhardbalansag@gmail.com>
2021-02-11 12:11:21 +0800

    remove button in view all aplicanats

--- resources/views/livewire/admin/reservation/view-all-reservation.blade.php
+++ resources/views/livewire/admin/reservation/view-all-reservation.blade.php
@@ -1,25 +1,4 @@
 <div>
-    <div class="w-25">
-        <form wire:submit.prevent="filterReservation">
-            <div class="my-2">
-                <label for="orderBy" class="block text-sm font-medium leading-5 text-gray-700">Order By</label>
-                <select wire:model.defer="filterBy" id="orderBy" class="block w-full p-2 px-3 py-2 mt-1 transition duration-150 ease-in-out bg-white border-gray-400 border-solid rounded-md shadow-sm border-1 form-select focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
-                    <option value="none">- Group By -</option>
-                    <option value="service_reservations.customerId">Customer</option>
-                    <option value="service_reservations.reservationDate">Reservation Date</option>
-                    <option value="service_reservations.status">Status</option>
-                </select>
-            </div>
-            <div class = "text-center ">
-                <button class="w-full btn btn-dark">Search product</button>
-            </div>
-        </form>
-    </div>
-    @if (session()->has('message'))
-        <div class="container text-center capitalize alert alert-success">
-            {{ session('message') }}
-        </div>
-    @endif
     <div class="my-4 text-center border-bottom border-dark row col-md-12 text-capitalize">
         <div class="col-md-3">
             Customer
